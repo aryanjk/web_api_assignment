f55fa9fcdfe13d1791cef633400421490850e6f4
aryanjk <karki.aryan54@gmail.com>
2024-05-05 22:27:22 +0545

    Commit changes

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-const express = required
+const express = require("express");
+const userRoutes = require('./routes/userRoutes')
+
+require("dotenv").config()
+const connectDatabase = require('./database/database')
+
+const app = express();
+
+app.use(express.json())
+app.use("/api/users", userRoutes)
+
+connectDatabase()
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
